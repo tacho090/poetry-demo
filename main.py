66b6b1f0feadea2e362397ce3d9ddc71be93ff69
Pablo Anastacio Mora Chatski <pmora1990@gmail.com>
2022-11-11 20:20:50 +0000

    add dotenv package and pymongo

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster25583.uxocfmt.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+
+MONGO_ATLAS_URI = os.environ.get("MONGO_ATLAS_URI")
+MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
+MONGO_PASSWORD = os.environ.get("MONGO_DB_PASSWORD")
+MONGO_DB_USERNAME = os.environ.get("MONGO_DB_USERNAME")
+
+load_dotenv()
+client = pymongo.MongoClient(f"mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_PASSWORD}@cluster25583.uxocfmt.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 db = client.test
